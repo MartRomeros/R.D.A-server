51e599cfaa1df54f2a78885d9d250666838262e8
MartinRomeros <mart.romeros@duocuc.cl>
2025-07-17 12:20:25 -0400

    verificando cambios

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -49,6 +49,8 @@ export const login = async (req: Request, res: Response): Promise<void> => {
             sameSite: 'none',
             maxAge: 24 * 60 * 60 * 1000
         })
+
+        console.log('Cookie enviada con options secure y sameSite none');
         
         res.status(200).json({ tipo_usuario_id: usuario.tipo_usuario_id })
 
